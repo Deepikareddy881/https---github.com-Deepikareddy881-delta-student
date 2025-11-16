ab817555d0a5022391f4252818eaafcfef4c3c93
Deepikareddy881 <deepikaendreddy757@gmail.com>
2025-11-16 19:26:35 +0530

    Update cloudConfig.js

--- cloudConfig.js
+++ cloudConfig.js
@@ -12,7 +12,7 @@ const storage = new CloudinaryStorage({
     cloudinary: cloudinary,
     params: {
       folder:'wanderlust_DEV',
-      allowedFormats:["png", "jpeg", "jpg"],
+      allowed_formats:["png", "jpeg", "jpg"],
     },
   });
 
