5e5e7fed4a0750ef92a539df5afdd1e1ba5b10a7
Deepikareddy881 <deepikaendreddy757@gmail.com>
2025-11-16 20:54:46 +0530

    Update app.js

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ async function main() {
 app.engine("ejs", ejsMate);
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
-app.set("layout", "layouts/boilerplate");
+//app.set("layout", "layouts/boilerplate");
 app.use(express.urlencoded({extended: true}));
 app.use(methodOverride("_method"));
 app.use(express.static(path.join(__dirname, "/public")));
