299c2c18fc2299f3211802bfee79c0cb9dfd88af
Deepikareddy881 <deepikaendreddy757@gmail.com>
2025-11-16 19:22:09 +0530

    Update app.js

--- app.js
+++ app.js
@@ -48,8 +48,7 @@ const store = MongoStore.create({
   },
   touchAfter: 24 * 3600,
 });
-
-store.on("error", () => {
+store.on("error", (err) => {  // FIX 2
   console.log("ERROR in MONGO SESSION STORE", err);
 });
 
